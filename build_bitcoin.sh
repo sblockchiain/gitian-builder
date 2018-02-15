932a0ced4ca2ae3de5ef6ab0c2ffce9f84e96b07
Your Name <you@example.com>
2018-02-15 18:44:22 +0100

    ok

---
+++ build_bitcoin.sh
@@ -0,0 +1,1 @@
+LXC_BRIDGE=lxcbr0 LXC_ARCH=amd64 LXC_SUITE=trusty bin/gbuild bitcoin-win.yml --commit bitcoin=7b57bc9 --url bitcoin=https://github.com/bitcoin/bitcoin
---
+++ build_lux.sh
@@ -0,0 +1,1 @@
+LXC_BRIDGE=lxcbr0 LXC_ARCH=amd64 LXC_SUITE=trusty bin/gbuild lux-win.yml --commit lux=eea64fa7c5be --url lux=https://github.com/sblockchiain/lux
